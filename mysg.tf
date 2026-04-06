646d477f3d44898a53f5201c5d5470cce8b5b444
vmanral <vijaymanral@hotmail.com>
2026-04-07 00:01:55 +1000

    Add remote state data source and output for sg_info
    Added remote state data source and new output for security group info.

--- mysg.tf
+++ mysg.tf
@@ -6,6 +6,18 @@ output "sg_id" {
   value = aws_security_group.test.id
 }
 
-output "sgp_name" {
-  value = aws_security_group.test.name
+data "terraform_remote_state" "foo" {
+  backend = "remote"
+
+  config = {
+    organization = "vmanral-org-1"
+
+    workspaces = {
+      name = "cli-driven-workflow"
+    }
+  }
+}
+
+output "sg_info" {
+  value = data.terraform_remote_state.foo.outputs
 }
